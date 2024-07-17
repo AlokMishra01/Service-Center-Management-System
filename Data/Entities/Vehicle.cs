dd659859e747fa9771c4085db4c01ce633d36d38
alok <alok.mishra@ebpearls.com>
2024-07-17 19:58:36 +0545

    Add project files.

---
+++ Data/Entities/Vehicle.cs
@@ -0,0 +1,10 @@
+﻿namespace Service_Center_Management_System.Data.Entities
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public string VinNumber { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+    }
+}
